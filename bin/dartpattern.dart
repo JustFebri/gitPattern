6c134137ad04c1b236f6c5831ee452ba24f352a9
Febri <febri.k150201@gmail.com>
2022-03-07 18:38:03 +0700

    Ver. 2.0

--- bin/dartpattern.dart
+++ bin/dartpattern.dart
@@ -1,7 +1,47 @@
 import 'dart:io';
 
 void nomor1(int n) {
-  print("Nomor 1 adalah $n");
+  int a, i, j, k, count = 0;
+  int num = 1;
+  a = (n % 2) + (n ~/ 2);
+  for (int i = 1; i <= a; i++) {
+    k = 1;
+    for (int j = a - i; j > 0; j--) {
+      stdout.write("*");
+    }
+    for (j = 0; j < i; j++) {
+      stdout.write(num);
+      if (k < i) {
+        stdout.write(" ");
+
+        k = k + 1;
+      }
+    }
+    for (int j = a - i; j > 0; j--) {
+      stdout.write("*");
+    }
+    num++;
+    stdout.write("\n");
+  }
+
+  for (i = n - a; i > 0; i--) {
+    k = 1;
+    for (int j = a - i; j > 0; j--) {
+      stdout.write("*");
+    }
+    for (j = 0; j < i; j++) {
+      stdout.write(num);
+      if (k < i) {
+        stdout.write(" ");
+        k = k + 1;
+      }
+    }
+    for (int j = a - i; j > 0; j--) {
+      stdout.write("*");
+    }
+    num++;
+    stdout.write("\n");
+  }
 }
 
 void nomor2(int n) {
